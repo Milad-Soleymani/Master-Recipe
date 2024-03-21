f064eb5bc8c03f91964bc641ecaa1d3e027e9ab9
MiladCoder <miladcoder1390@gmail.com>
2024-03-21 16:05:22 +0330

    add Navbar items and basic styles

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,19 @@
+import Logo from '../assets/images/MasterRecipe.png'
+import '../Styles/NavBar.css'
+
 const NavBar = () => {
-    return(
-        
+    return (
+        <div className="nav">
+            <div className="right-div">
+                <h1>
+                    Master Recipe
+                </h1>
+            </div>
+            <div className="left-div">
+                <img src={Logo} className='NavImg' alt="Master Recipe" />
+            </div>
+        </div>
     )
 }
+
+export default NavBar
