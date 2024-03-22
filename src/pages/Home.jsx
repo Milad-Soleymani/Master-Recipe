578c28ac000f7750b395eb1d877814569000acf5
MiladCoder <miladcoder1390@gmail.com>
2024-03-22 14:35:35 +0330

    add text and styles for Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,17 @@
+import '../Styles/Home.css'
+
 const Home = () => {
- return(
-    <h1> Master Recipe </h1>
- )   
+    return (
+        <div>
+            <h1 className='header'> Hi, Welcome to Master Recipe Website </h1>
+            <p> You can view most popular food, recipes and cook.
+                You view Most popular foods below under 
+                </p>
+                <h2 className='MoostPoularHeader'>
+                    Must Popular
+                </h2>
+        </div>
+    )
 }
 
 export default Home;
