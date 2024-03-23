581a67dbbeb38dca53ef4f12e94cf5f72ae05abb
MiladCoder <miladcoder1390@gmail.com>
2024-03-23 11:59:20 +0330

    add burger card

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,36 +9,38 @@ const Home = () => {
         <div>
             <h1 className='header'> Hi, Welcome to Master Recipe Website </h1>
             <p> You can view most popular food, recipes and cook.
-                You view Most popular foods below under 
-                </p>
-                <h2 className='MoostPoularHeader'>
-                    Must Popular
-                </h2>
-                <div className='Cards'>
+                You view Most popular foods below under
+            </p>
+            <h2 className='MoostPoularHeader'>
+                Must Popular
+            </h2>
+            <div className='Cards'>
 
-                    <div className="Card1 Card">
-                        <img src={pizzaPepperoni} />
-                        <h3> pizza pepperoni </h3>
-                        <p> Pepperoni pizza is one of the best foods. 55% of people put this food in their favorite list </p>
-                        <a href=""> Get Recipe </a>
-                    </div>
-                    
-                    <div className="Card2 Card">
-                        
+                <div className="Card1 Card">
+                    <img src={pizzaPepperoni} />
+                    <h3> pizza pepperoni </h3>
+                    <p> Pepperoni pizza is one of the best foods. 55% of people put this food in their favorite list </p>
+                    <a href=""> Get Recipe </a>
+                </div>
 
-                    </div>
-                    
-                    <div className="Card3 Card">
-                        
+                <div className="Card2 Card">
+                    <img src={burger} />
+                    <h3> burger </h3>
+                    <p> Next to pizza, burger is one of the best foods in the world and many people like it very much </p>
+                    <a href=""> Get Recipe </a>
+                </div>
 
-                    </div>
-                    
-                    <div className="Card4 Card">
-                        
+                <div className="Card3 Card">
 
-                    </div>
 
                 </div>
+
+                <div className="Card4 Card">
+
+
+                </div>
+
+            </div>
         </div>
     )
 }
