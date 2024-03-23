370cf9292123a3565c2013b316ae8c2026c3ba2a
MiladCoder <miladcoder1390@gmail.com>
2024-03-23 11:50:48 +0330

    add Most poular Cards Section and add pizza pepperoni

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,9 @@
 import '../Styles/Home.css'
 
+
+import pizzaPepperoni from '../assets/images/pizza-pepperoni.jpeg'
+import burger from '../assets/images/burger.jpg'
+
 const Home = () => {
     return (
         <div>
@@ -10,6 +14,31 @@ const Home = () => {
                 <h2 className='MoostPoularHeader'>
                     Must Popular
                 </h2>
+                <div className='Cards'>
+
+                    <div className="Card1 Card">
+                        <img src={pizzaPepperoni} />
+                        <h3> pizza pepperoni </h3>
+                        <p> Pepperoni pizza is one of the best foods. 55% of people put this food in their favorite list </p>
+                        <a href=""> Get Recipe </a>
+                    </div>
+                    
+                    <div className="Card2 Card">
+                        
+
+                    </div>
+                    
+                    <div className="Card3 Card">
+                        
+
+                    </div>
+                    
+                    <div className="Card4 Card">
+                        
+
+                    </div>
+
+                </div>
         </div>
     )
 }
