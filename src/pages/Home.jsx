c847a7fc710bdb6d2c2de650f5b91f32fe5829a7
MiladCoder <miladcoder1390@gmail.com>
2024-03-23 15:36:31 +0330

    add alt to images

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,21 +18,21 @@ const Home = () => {
             <div className='Cards'>
 
                 <div className="Card1 Card">
-                    <img src={pizzaPepperoni} />
+                    <img src={pizzaPepperoni} alt='Pizza Pepperoni'/>
                     <h3> pizza pepperoni </h3>
                     <p> Pepperoni pizza is one of the best foods. 55% of people put this food in their favorite list </p>
                     <a href=""> Get Recipe </a>
                 </div>
 
                 <div className="Card2 Card">
-                    <img src={burger} />
+                    <img src={burger} alt='burger'/>
                     <h3> burger </h3>
                     <p> Next to pizza, burger is one of the best foods in the world and many people like it  </p>
                     <a href=""> Get Recipe </a>
                 </div>
 
                 <div className="Card3 Card">
-                    <img src={sushi} />
+                    <img src={sushi} alt='Sushi'/>
                     <h3> Sushi </h3>
                     <p>Sushi is a Japanese dish made of vinegared rice that is usually accompanied by meat </p>
                     <a href=""> Get Recipe </a>
