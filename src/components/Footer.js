83ef328bca11d7e6c0f66e55a8598b726211fd24
MiladCoder <miladcoder1390@gmail.com>
2024-03-24 20:17:42 +0330

    partioning footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,13 @@ const Footer = () => {
 
     return (
         <div className="footer">
+            <div className='left-part'>
             <h4> All right reserved  
                 copyright 2024 	&#169;
             </h4>
+            </div>
+            <div className='right-part'>
+            </div>
         </div>
     )
 }
