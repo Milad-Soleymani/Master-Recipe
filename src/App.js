6c827942c8a39ab5caea81f31fc4c4b4ca1f7769
MiladCoder <miladcoder1390@gmail.com>
2024-03-25 11:34:06 +0330

    Add Route for pizza pepperoni page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import NavBar from './components/NavBar'
 
 import Home from './pages/Home'
 import Footer from './components/Footer'
+import Pepproni from './pages/pepperoni';
+
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
@@ -14,6 +16,7 @@ function App() {
         <NavBar />
         <Routes>
           <Route path='/' element={<Home />}/>
+          <Route path='/recipe/pepperoni' element={<Pepproni />} />
         </Routes>
         <Footer />
       </Router>
