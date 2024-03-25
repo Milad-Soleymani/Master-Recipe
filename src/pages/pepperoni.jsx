17590c533ea65730d266377ada825c91d6712523
MiladCoder <miladcoder1390@gmail.com>
2024-03-25 12:16:35 +0330

    Add Header for pepperoni page

--- src/pages/pepperoni.jsx
+++ src/pages/pepperoni.jsx
@@ -1,10 +1,36 @@
+import pepperoni from '../assets/images/pepperoni.jpg'
+
+import '../Styles/pepperoni.css'
+
 const Pepproni = () => {
 
-    return(
+    return (
         <div>
-
+            <div className='Header'>
+                <div className="picture">
+                    <img src={pepperoni} className='pepperoniImage' alt='pepperoni' />
+                </div>
+                <div className="details">
+                    <h2> Pizza Pepperoni </h2>
+                    <ul>
+                        <li> Lorem ipsum dolor sit amet. </li>
+                        <li> Lorem ipsum dolor sit amet. </li>
+                        <li> Lorem ipsum dolor sit amet. </li>
+                        <li> Lorem ipsum dolor sit amet. </li>
+                        <li> Lorem ipsum dolor sit amet. </li>
+                        <li> Lorem ipsum dolor sit amet. </li>
+                        <li> Lorem ipsum dolor sit amet. </li>
+                        <li> Lorem ipsum dolor sit amet. </li>
+                        <li> Lorem ipsum dolor sit amet. </li>
+                        <li> Lorem ipsum dolor sit amet. </li>
+                        <li> Lorem ipsum dolor sit amet. </li>
+                    </ul>
+                </div>
+            </div>
+            <div className="Ingredients"> </div>
+            <div className="How-to-cook"></div>
         </div>
     )
-} 
+}
 
 export default Pepproni;
