6f7abbae7bdd6e26756bdd19ee10189710f09dac
MiladCoder <miladcoder1390@gmail.com>
2024-03-26 10:49:09 +0330

    adding object

--- src/pages/baklava.jsx
+++ src/pages/baklava.jsx
@@ -1,8 +1,44 @@
+import BaklavaImg from '../assets/images/Baklava.jpg'
+
 const Baklava = () => {
 
-    return(
+    return (
         <div>
-            
+            <div className='Header'>
+                <div className="picture">
+                    <img src={BaklavaImg} width='800px' className='BaklavaImage' alt='Baklava' />
+                </div>
+                <div className="details">
+                    <h2>  Baklava </h2>
+                    <h4> Cook Time :    1.5 hours </h4>
+                    <ul>
+                        <li> Lorem ipsum dolor sit amet. </li>
+                        <li> Lorem ipsum dolor sit amet. </li>
+                        <li> Lorem ipsum dolor sit amet. </li>
+                        <li> Lorem ipsum dolor sit amet. </li>
+                        <li> Lorem ipsum dolor sit amet. </li>
+                        <li> Lorem ipsum dolor sit amet. </li>
+                        <li> Lorem ipsum dolor sit amet. </li>
+                        <li> Lorem ipsum dolor sit amet. </li>
+                        <li> Lorem ipsum dolor sit amet. </li>
+                        <li> Lorem ipsum dolor sit amet. </li>
+                        <li> Lorem ipsum dolor sit amet. </li>
+                    </ul>
+                </div>
+            </div>
+            <div className="How-to-cook">
+                <h4> Step 1 </h4>
+                <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti libero natus eveniet magnam sint ratione ea sequi<br /> eius amet distinctio esse recusandae repellat. Sunt quod fuga atque, alias explicabo cupiditate! </p>
+                <br /><h4> step 2 </h4>
+                <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti libero natus eveniet magnam sint ratione ea sequi<br /> eius amet distinctio esse recusandae repellat. Sunt quod fuga atque, alias explicabo cupiditate! </p>
+                <br /><h4> Step 3 </h4>
+                <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti libero natus eveniet magnam sint ratione ea sequi<br /> eius amet distinctio esse recusandae repellat. Sunt quod fuga atque, alias explicabo cupiditate! </p>
+                <br /><h4> Step 4 </h4>
+                <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti libero natus eveniet magnam sint ratione ea sequi<br /> eius amet distinctio esse recusandae repellat. Sunt quod fuga atque, alias explicabo cupiditate! </p>
+                <br /><h4> Step 5 </h4>
+                <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti libero natus eveniet magnam sint ratione ea sequi<br /> eius amet distinctio esse recusandae repellat. Sunt quod fuga atque, alias explicabo cupiditate! </p>
+                <h1> Now, can eat <BsEmojiHeartEyes /> </h1>
+            </div>
         </div>
     )
 }
