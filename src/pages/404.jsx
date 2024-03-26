6f9d79d290520d398f303a2829bf4650a45ff553
MiladCoder <miladcoder1390@gmail.com>
2024-03-26 11:18:38 +0330

    add 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,8 +1,14 @@
+import '../Styles/404.css'
+
+import { TbError404 } from 'react-icons/tb'
+
 const NotFound = () => {
 
     return(
-        <div>
-            
+        <div className='NFound'>
+            <TbError404 fontSize='350px' />
+            <h3 > 404 Not Found </h3>
+            <a target='_self' href='/'> Back to Home </a>
         </div>
     )
 }
